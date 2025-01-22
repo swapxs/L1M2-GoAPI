5b146205dc19142d30268d170757a3b16920f032
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-22 15:08:50 +0530

    basic go-gin server setup

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"swapxs/api_proj/pkg/database"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	database.DBInit()
+	defer database.CloseDB()
+
+	r := gin.Default()
+
+	if e := r.Run(":8080"); e != nil {
+		log.Fatalf("Failed to start server: %v", e)
+	}
+}
