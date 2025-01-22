b75aa48b7fd930c3e913c8053b3c756ec8824952
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-22 14:45:15 +0530

    structure for the JSON db

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,3 +1,9 @@
 package models
 
-
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	DueDate     string `json:"dueDate"`
+	Status      string `json:"status"`
+}
