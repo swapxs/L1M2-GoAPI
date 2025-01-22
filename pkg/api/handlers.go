9640544009e436c7f62c2cb04e49421c23b29bc7
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-22 15:40:56 +0530

    init config with createtask function

--- pkg/api/handlers.go
+++ pkg/api/handlers.go
@@ -1,1 +1,28 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+	"swapxs/api_proj/pkg/database"
+	"swapxs/api_proj/pkg/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateTask(c *gin.Context) {
+	var newTask models.Task
+
+	if e := c.BindJSON(&newTask); e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Invalid Request: %v", e)})
+		return
+	}
+
+	t, e := database.InsertTask(newTask)
+
+	if e != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to create task: %v", e)})
+		return
+	}
+
+	c.JSON(http.StatusCreated, t)
+}
