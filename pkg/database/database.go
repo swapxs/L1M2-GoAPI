f524c49cf9613ad42339c121100cfac3d518dc2f
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-22 14:46:10 +0530

    Created function that:
    1. Initializes DB
    2. Closes DB
    3. Create a new entry in the DB

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,1 +1,59 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"swapxs/api_proj/pkg/models"
+)
+
+var db *sql.DB
+
+// DB Initialize/Close
+func DBInit() {
+	var e error
+	db, e := sql.Open("sqlite3", "tasks.db")
+
+	if e != nil {
+		log.Fatalf("failed to open database: %v", e)
+	}
+
+	tC := `
+		CREATE TABLE IF NOT EXISTS tasks (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT,
+			description TEXT,
+			dueDate TEXT,
+			status TEXT
+		)
+	`
+	_, e = db.Exec(tC)
+
+	if e != nil {
+		log.Fatalf("\nFailed to create table: %v\n", e)
+	}
+}
+
+func CloseDB() {
+	if db != nil {
+		db.Close()
+	}
+}
+
+func createTask(t models.Task) (models.Task, error) {
+	res, e := db.Exec("INSERT INTO tasks (title, description, dueDate, status) VALUES (?, ?, ?, ?)",
+		t.Title, t.Description, t.DueDate, t.Status)
+
+	if e != nil {
+		return models.Task{}, fmt.Errorf("\nFailed to create new task.\nERROR: %v\n", e)
+	}
+
+	id, e := res.LastInsertId()
+
+	if e != nil {
+		return models.Task{}, fmt.Errorf("\nFailed to get task id.\nERROR: %v\n", e)
+	}
+
+	t.ID = int(id)
+	return t, nil
+}
