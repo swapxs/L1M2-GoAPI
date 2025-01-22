af66233b38001f637880614313efb5f69f856caa
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-22 16:23:21 +0530

    new update and delete function and minor name changes

--- pkg/api/handlers.go
+++ pkg/api/handlers.go
@@ -3,12 +3,14 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"swapxs/api_proj/pkg/database"
 	"swapxs/api_proj/pkg/models"
 
 	"github.com/gin-gonic/gin"
 )
 
+// CRUD
 func CreateTask(c *gin.Context) {
 	var newTask models.Task
 
@@ -17,7 +19,7 @@ func CreateTask(c *gin.Context) {
 		return
 	}
 
-	t, e := database.InsertTask(newTask)
+	t, e := database.CreateTask(newTask)
 
 	if e != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to create task: %v", e)})
@@ -25,4 +27,43 @@ func CreateTask(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, t)
+}
+
+func Update(c *gin.Context) {
+	var updateTask models.Task
+	getIdPara := c.Param("id")
+	
+	id, e := strconv.Atoi(getIdPara)
+
+	if e != nil {
+		// error
+		return
+	}
+
+	t, e := database.UpdateTask(id, updateTask)
+
+	if e != nil {
+		return
+	}
+
+	c.JSON(http.StatusOK, t)
+}
+
+func Delete(c *gin.Context) {
+	getIdPara := c.Param("id")
+	
+	id, e := strconv.Atoi(getIdPara)
+
+	if e != nil {
+		// error
+		return
+	}
+
+	e = database.DeleteTask(id)
+
+	if e != nil {
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "task deleted successfully"})
 }
