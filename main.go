90be0d0d65e8c822754d51f934a6ed107d8a685f
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-23 09:03:38 +0530

    BUG FIX: 404 not found; so stated the server after deploying endpoints

--- main.go
+++ main.go
@@ -13,14 +13,15 @@ func main() {
 
 	r := gin.Default()
 
-	if e := r.Run(":8080"); e != nil {
-		log.Fatalf("Failed to start server: %v", e)
-	}
-
 	// Part 1
 	r.POST("/tasks", api.Create)
 	// Part 3
 	r.PUT("/tasks/:id", api.Update)
 	// Part 4
 	r.DELETE("/tasks/:id", api.Delete)
+
+	if e := r.Run(":8080"); e != nil {
+		log.Fatalf("Failed to start server: %v", e)
+	}
+
 }
