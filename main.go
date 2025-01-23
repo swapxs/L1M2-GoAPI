c0277e141271c925e772b991f03b50b677c3e76c
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-23 08:08:11 +0530

    added the functions that are in handlers.go

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"swapxs/api_proj/pkg/api"
 	"swapxs/api_proj/pkg/database"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,4 +17,8 @@ func main() {
 	if e := r.Run(":8080"); e != nil {
 		log.Fatalf("Failed to start server: %v", e)
 	}
+
+	r.POST("/tasks", api.Create)
+	r.PUT("/tasks/:id", api.Update)
+	r.DELETE("/tasks/:id", api.Delete)
 }
