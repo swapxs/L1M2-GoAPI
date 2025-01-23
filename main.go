95aac74bd4f449bf414b10f60395ede2e91e06f0
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-23 11:29:32 +0530

    Properly Documented The file

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+/* Created by Swapnil Bhowmik (XS/IN/0893) for Go API Task in L1: Module 2 
+* Main file for the entire project, this file performs the following:
+* 1. Initializes the database
+* 2. Creates a Router r using gin
+* 3. Performs all the required tasks outlined in the document
+* 4. Runs the server and checks for errors.
+* 5. Closes the Database after it is finished executing.*/
+
 package main
 
 import (
