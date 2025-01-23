4883efd78b4a2ff538194a91ad21d5e36736ed28
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-23 08:27:58 +0530

    renamed these for better readability

--- pkg/models/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	DueDate     string `json:"dueDate"`
-	Status      string `json:"status"`
-}
