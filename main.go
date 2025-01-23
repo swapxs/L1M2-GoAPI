14fb7fdaee4223edf786aa71f17cbe16100c0244
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-23 11:29:09 +0530

    Completed all the required tasks by adding Part 2 and 5

--- main.go
+++ main.go
@@ -15,13 +15,20 @@ func main() {
 
 	// Part 1
 	r.POST("/tasks", api.Create)
+
+	// Part 2
+	r.GET("/tasks/:id", api.Read)
+
 	// Part 3
 	r.PUT("/tasks/:id", api.Update)
+
 	// Part 4
 	r.DELETE("/tasks/:id", api.Delete)
+
+	// Part 5
+	r.GET("/tasks", api.ReadAll)
 
 	if e := r.Run(":8080"); e != nil {
 		log.Fatalf("Failed to start server: %v", e)
 	}
-
 }
