b0b4eff9316dc3e5132994f7c5ea97a6f8784f37
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-23 11:33:57 +0530

    documented the file properly

--- pkg/format/format.go
+++ pkg/format/format.go
@@ -1,3 +1,7 @@
+/* Created by Swapnil Bhowmik (XS/IN/0893) for Go API Task in L1: Module 2 
+* This files makes a struct that mimics the JSON structure for the database
+* operations */
+
 package format
 
 type Task struct {
