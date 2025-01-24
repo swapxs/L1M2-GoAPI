dd5a837d7e7c4a7f6f6080da79504dea021e3d7c
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-24 15:48:49 +0530

    better implementation of the style guide

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /* Created by Swapnil Bhowmik (XS/IN/0893) for Go API Task in L1: Module 2
 * Main file for the entire project, this file performs the following:
 * 1. Initializes the database
-* 2. Creates a Router r using gin
+* 2. Creates a Router using gin
 * 3. Performs all the required tasks outlined in the document
 * 4. Runs the server and checks for errors.
 * 5. Closes the Database after it is finished executing.*/
@@ -11,32 +11,23 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
-	"swapxs/GoAPI/pkg/api"
-	"swapxs/GoAPI/pkg/database"
+	"github.com/swapxs/GoAPI/pkg/api"
+	"github.com/swapxs/GoAPI/pkg/db"
 )
 
 func main() {
-	database.DBInit()
-	defer database.CloseDB()
+	db.Init()
+	defer db.Close()
 
 	r := gin.Default()
 
-	// Part 1
 	r.POST("/tasks", api.Create)
-
-	// Part 2
 	r.GET("/tasks/:id", api.Read)
-
-	// Part 3
 	r.PUT("/tasks/:id", api.Update)
-
-	// Part 4
 	r.DELETE("/tasks/:id", api.Delete)
-
-	// Part 5
 	r.GET("/tasks", api.ReadAll)
 
-	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	if e := r.Run(":8080"); e != nil {
+		log.Fatalf("Failed to start server: %v", e)
 	}
 }
