b41f63f3a89b2f405484b4c57f2f93fa3c26edeb
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-24 15:49:30 +0530

    better implementation of the style guide

--- pkg/api/functions.go
+++ pkg/api/functions.go
@@ -12,152 +12,35 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"swapxs/GoAPI/pkg/database"
-	"swapxs/GoAPI/pkg/format"
+	"github.com/swapxs/GoAPI/pkg/db"
+	"github.com/swapxs/GoAPI/pkg/format"
 	"time"
 )
 
-// CRUD Operations
-// 1. Create new data
-func Create(c *gin.Context) {
-	var newTask format.Task
+// Data Validation
+func IsValidDate(d string) bool {
+	_, e := time.Parse("2006-01-02", d)
 
-	if err := c.BindJSON(&newTask); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": fmt.Sprintf("Invalid Request: %v", err)})
-		return
-	}
-
-	log.Printf("Task received: %+v ", newTask)
-
-	if err := IsValidTask(newTask); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
-		return
-	}
-
-	t, err := database.CreateTask(newTask)
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to create task: %v", err)})
-		return
-	}
-
-	c.JSON(http.StatusCreated, t)
+	return e == nil
 }
 
-// 2. Read specified data & ReadAll data
-func Read(c *gin.Context) {
-	getIdPara := c.Param("id")
-
-	id, err := strconv.Atoi(getIdPara)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid task ID"})
-		return
-	}
-
-	t, err := database.GetTaskID(id)
-
-	if err != nil {
-		if err.Error() == "task not found" {
-			c.JSON(http.StatusNotFound, gin.H{"Error": "Task not found"})
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to get task: %v", err)})
-		}
-		return
-	}
-
-	c.JSON(http.StatusOK, t)
-}
-
-func ReadAll(c *gin.Context) {
-	t, err := database.GetAllTasks()
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to get all tasks: %v", err)})
-		return
-	}
-
-	c.JSON(http.StatusOK, t)
-}
-
-// 3. Update specified data
-func Update(c *gin.Context) {
-	var updateTask format.Task
-	getIdPara := c.Param("id")
-
-	id, err := strconv.Atoi(getIdPara)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid task ID"})
-		return
-	}
-
-	if err := c.BindJSON(&updateTask); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": fmt.Sprintf("Invalid request: %v", err)})
-		return
-	}
-
-	if err := IsValidTask(updateTask); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
-		return
-	}
-
-	t, err := database.UpdateTask(id, updateTask)
-
-	if err != nil {
-		if err.Error() == "Task not found" {
-			c.JSON(http.StatusNotFound, gin.H{"Error": "Task not found"})
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to get task: %v", err)})
-		}
-		return
-	}
-
-	c.JSON(http.StatusOK, t)
-}
-
-// 3. Delete specified data
-func Delete(c *gin.Context) {
-	getIdPara := c.Param("id")
-
-	id, err := strconv.Atoi(getIdPara)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid task ID"})
-		return
-	}
-
-	err = database.DeleteTask(id)
-
-	if err != nil {
-		if err.Error() == "Task not found 404" {
-			c.JSON(http.StatusNotFound, gin.H{"Error": "Task not found"})
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to delete task: %v", err)})
-		}
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"Message": "Task deleted successfully"})
-}
-
-func IsValidTask(t format.Task) error {
+func IsValidTask(task format.Task) error {
 	switch {
-	case strings.TrimSpace(t.Title) == "":
+	case strings.TrimSpace(task.Title) == "":
 		return fmt.Errorf("Title cannot be empty")
 
-	case strings.TrimSpace(t.Status) == "":
+	case strings.TrimSpace(task.Status) == "":
 		return fmt.Errorf("Status cannot be empty")
 
-	case strings.ToLower(t.Status) != "pending" &&
-		strings.ToLower(t.Status) != "in progress" &&
-		strings.ToLower(t.Status) != "completed":
+	case strings.ToLower(task.Status) != "pending" &&
+		strings.ToLower(task.Status) != "in progress" &&
+		strings.ToLower(task.Status) != "completed":
 		return fmt.Errorf("Invalid status, it should be 'pending', 'in progress' or 'completed'")
 
-	case strings.TrimSpace(t.DueDate) == "":
+	case strings.TrimSpace(task.DueDate) == "":
 		return fmt.Errorf("Due date cannot be empty")
 
-	case !IsValidDate(t.DueDate):
+	case !IsValidDate(task.DueDate):
 		return fmt.Errorf("Invalid date format: Please use YYYY-MM-DD")
 
 	default:
@@ -165,8 +48,126 @@ func IsValidTask(t format.Task) error {
 	}
 }
 
-func IsValidDate(d string) bool {
-	_, err := time.Parse("2006-01-02", d)
+// CRUD Operations
+// 1. Create new data
+func Create(c *gin.Context) {
+	var newTask format.Task
 
-	return err == nil
+	if e := c.BindJSON(&newTask); e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": fmt.Sprintf("Invalid Request: %v", e)})
+		return
+	}
+
+	if e := IsValidTask(newTask); e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": e.Error()})
+		return
+	}
+
+	log.Printf("Task received: %+v ", newTask)
+
+	task, e := db.CreateTask(newTask)
+
+	if e != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to create task: %v", e)})
+		return
+	}
+
+	c.JSON(http.StatusCreated, task)
+}
+
+// 2. Read specified data & ReadAll data
+func Read(c *gin.Context) {
+	getId := c.Param("id")
+
+	id, e := strconv.Atoi(getId)
+
+	if e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid task ID"})
+		return
+	}
+
+	task, e := db.GetTaskID(id)
+
+	if e != nil {
+		if e.Error() == "Task not found" {
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Task not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to get task: %v", e)})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+func ReadAll(c *gin.Context) {
+	task, e := db.GetAllTasks()
+
+	if e != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to get all tasks: %v", e)})
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+// 3. Update specified data
+func Update(c *gin.Context) {
+	var updateTask format.Task
+	getId := c.Param("id")
+
+	id, e := strconv.Atoi(getId)
+
+	if e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid task ID"})
+		return
+	}
+
+	if e := c.BindJSON(&updateTask); e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": fmt.Sprintf("Invalid request: %v", e)})
+		return
+	}
+
+	if e := IsValidTask(updateTask); e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": e.Error()})
+		return
+	}
+
+	task, e := db.UpdateTask(id, updateTask)
+
+	if e != nil {
+		if e.Error() == "Task not found" {
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Task not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to get task: %v", e)})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+// 3. Delete specified data
+func Delete(c *gin.Context) {
+	getId := c.Param("id")
+
+	id, e := strconv.Atoi(getId)
+
+	if e != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid task ID"})
+		return
+	}
+
+	e = db.DeleteTask(id)
+
+	if e != nil {
+		if e.Error() == "Task not found 404" {
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Task not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": fmt.Sprintf("Failed to delete task: %v", e)})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"Message": "Task deleted successfully"})
 }
